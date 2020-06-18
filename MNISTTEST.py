cce7ca4052bad67011f071686714af580ac5a72c
kakeru58 <kakeru58@users.noreply.github.com>
2020-06-18 22:07:48 +0900

    Anomaly detection

--- MNISTTEST.py
+++ MNISTTEST.py
@@ -7,13 +7,15 @@ from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 from keras import regularizers
 from keras import backend as K
+import cv2
 # MNISTデータを前処理する
-(x_train, _), (x_test, _) = mnist.load_data()  # 使わないため_に
+(x_train, y_train), (x_test, y_test) = mnist.load_data()  # 使わないため_に
 img_size = x_train.shape[1]
-x_train, x_valid = train_test_split(x_train, test_size=0.175)  # 訓練ようと試験用に分ける0.175は割合 過学習防止
+x_train, x_valid, y_train, y_valid = train_test_split(x_train, y_train, test_size=0.175)  # 訓練ようと試験用に分ける0.175は割合 過学習防止
 x_train = x_train.astype('float32') / 255.  # int型を画素ちの最大255でわる．(正規化)
 x_valid = x_valid.astype('float32') / 255.
 x_test = x_test.astype('float32') / 255.
+
 x_train = np.reshape(x_train, (len(x_train), img_size, img_size, 1))  # 3次元データを2次元データにlenは要素数
 x_valid = np.reshape(x_valid, (len(x_valid), img_size, img_size, 1))  # np.prodで1番目からの要素の積(784)
 x_test = np.reshape(x_test, (len(x_test), img_size, img_size, 1))
@@ -23,8 +25,8 @@ x = input_img = Input(shape=(img_size, img_size, 1))  # 28*28
 batch_size = 64
 latent_dim = 2
 epoch = 50
-x = Conv2D(32, (3, 3), activation='relu', strides=2, padding='same')(x)
-x = Conv2D(64, (3, 3), activation='relu', strides=2, padding='same')(x)
+x = Conv2D(32, (3, 3), activation='relu', strides=1, padding='same')(x)
+x = Conv2D(64, (3, 3), activation='relu', strides=1, padding='same')(x)
 
 shape = K.int_shape(x)
 x = Flatten()(x)
@@ -52,9 +54,8 @@ latent_inputs = Input(shape=(latent_dim,), name='z_sampling')
 x = Dense(shape[1] * shape[2] * shape[3], activation='relu')(latent_inputs)
 x = Reshape((shape[1], shape[2], shape[3]))(x)
 
-
-x = Conv2DTranspose(64, (3, 3), activation='relu', strides=2, padding='same')(x)
-x = Conv2DTranspose(32, (3, 3), activation='relu', strides=2, padding='same')(x)
+x = Conv2DTranspose(64, (3, 3), activation='relu', strides=1, padding='same')(x)
+x = Conv2DTranspose(32, (3, 3), activation='relu', strides=1, padding='same')(x)
 decoded = Conv2DTranspose(1, (3, 3), activation='sigmoid', padding='same')(x)
 
 decoder = Model(latent_inputs, decoded)
@@ -75,6 +76,8 @@ vae_loss = K.mean(reconstruction_loss + kl_loss)
 autoencoder.add_loss(vae_loss)
 autoencoder.compile(optimizer='adam', loss='')
 autoencoder.summary()
+x_train = x_train[y_train == 1]
+x_valid = x_valid[y_valid == 1]
 # autoencoderの実行
 history = autoencoder.fit(x_train,  # kerasのfitメゾット
                           epochs=epoch,
@@ -84,17 +87,28 @@ history = autoencoder.fit(x_train,  # kerasのfitメゾット
 decoded_img = autoencoder.predict(x_test)
 
 n = 10  # (表示個数)
-plt.figure(figsize=(20, 4))  # 横インチ縦インチの大きさ
+plt.figure(figsize=(20, 6))  # 横インチ縦インチの大きさ
 for i in range(n):
-    # オリジナルのテスト画像
-    ax = plt.subplot(2, n, i + 1)  # 何行何列何番目
-    plt.imshow(x_test[i].reshape(28, 28))
+    # オリジナル
+    origin_img = x_test[i].reshape(28, 28)
+    ax = plt.subplot(3, n, i + 1)  # 何行何列何番目
+    plt.imshow(origin_img)
     plt.gray()
     ax.get_xaxis().set_visible(False)  # 軸の値非表示
     ax.get_yaxis().set_visible(False)
     # 変換された画像
-    ax = plt.subplot(2, n, i + 1 + n)
-    plt.imshow(decoded_img[i].reshape(28, 28))
+    change_img = decoded_img[i].reshape(28, 28)
+    ax = plt.subplot(3, n, i + 1 + n)
+    plt.imshow(change_img)
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+    # 差分
+    diff_img = origin_img - change_img
+    diff_img_abs = np.abs(diff_img)
+    diff_img = diff_img_abs * 255
+    ax = plt.subplot(3, n, i + 1 + 2 * n)
+    plt.imshow(diff_img)
     plt.gray()
     ax.get_xaxis().set_visible(False)
     ax.get_yaxis().set_visible(False)
